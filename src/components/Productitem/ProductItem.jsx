497e19ee330a3ecd155791305f41d80ebe3fd737
crock444 <ifriauf@inbox.ru>
2024-08-01 20:28:46 +0700

    init

--- src/components/Productitem/ProductItem.jsx
+++ src/components/Productitem/ProductItem.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from "../Button/Button";
 import './ProductItem.css';
 
-const ProductItem = ({product, className, onAdd}) => {
+const ProductItem = ({ product, className, onAdd }) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     const onAddHandler = () => {
         onAdd(product);
-    }
+    };
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
 
     return (
         <div className={`product ${className}`}>
-            <div className={'img'}>
+            <div className={'img'} onDoubleClick={openModal}>
                 <img src={product.imageUrl} alt={product.title} />
             </div>
             <div className={'title'}>{product.title}</div>
@@ -21,6 +30,16 @@ const ProductItem = ({product, className, onAdd}) => {
             <Button className={'add-btn'} onClick={onAddHandler}>
                 Добавить в корзину
             </Button>
+
+            {/* Модальное окно */}
+            {isModalOpen && (
+                <div className="modal-overlay" onClick={closeModal}>
+                    <div className="modal-content" onClick={e => e.stopPropagation()}>
+                        <img src={product.imageUrl} alt={product.title} className="modal-image" />
+                        <button className="modal-close" onClick={closeModal}>Закрыть</button>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
