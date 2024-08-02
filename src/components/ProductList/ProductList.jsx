9ea6f7d6596192703c95775e6cb2da0b3c2d39f1
crock444 <ifriauf@inbox.ru>
2024-08-02 14:44:13 +0700

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -64,8 +64,9 @@ const ProductList = () => {
         } else {
             tg.MainButton.show();
             tg.MainButton.setParams({
-                text: `Купить ${getTotalPrice(newItems)}`
-            })
+                text: `Купить ${getTotalPrice(newItems)} руб`
+            });
+
         }
     }
 
